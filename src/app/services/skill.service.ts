b7aa1d7c7e16effb971a802967821cbf6aa1a246
Franze_Dev <50424852+ikytussistemas@users.noreply.github.com>
2021-05-12 09:12:48 -0300

    Update skill.service.ts

--- src/app/services/skill.service.ts
+++ src/app/services/skill.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Skill } from '../models/skill';
 
 @Injectable({
@@ -7,15 +7,17 @@ import { Skill } from '../models/skill';
 })
 export class SkillService {
  
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' })};
+
   constructor(
     private http: HttpClient,
   ) {}
 
   updateSkill(skill: Skill) {
-    return this.http.put('/api/skills', skill);
+    return this.http.put<Skill>('/api/skills', skill, this.httpOptions);
   }
 
   findAll() {
-    return this.http.get('/api/skills');
+    return this.http.get<Skill[]>('/api/skills');
   }
 }
